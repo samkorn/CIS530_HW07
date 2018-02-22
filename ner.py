31372df56673b205c3b58a28a14e0541ec5d6a23
SamKorn <samkorn@sas.upenn.edu>
2018-02-21 20:37:19 -0500

    -refactored feature/label generation method -added wrongs.py to evaluate examples with mistakes -added a POS feature -tested several parameters and features (results in comparisons.txt)

--- ner.py
+++ ner.py
@@ -1,88 +1,121 @@
 from nltk.corpus import conll2002
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import Perceptron
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import precision_recall_fscore_support
+import numpy as np
 
 # Assignment 7: NER
 # This is just to help you get going. Feel free to
 # add to or modify any part of it.
 
 
-def getfeats(word, o):
+def get_words_in_window(word_tuple, o):
     """ This takes the word in question and
     the offset with respect to the instance
     word """
     o = str(o)
-    features = [
-        (o + 'word', word)
-        # TODO: add more features here.
-    ]
+    features = [(o + 'word', word_tuple[0])]
     return features
-    
+
+
+def is_proper_case(word):
+    if word[0] == word[0].upper():
+        return 1
+    else:
+        return 0
+
 
 def word2features(sent, i):
     """ The function generates all features
     for the word at position i in the
     sentence."""
     features = []
-    # the window around the token
+
+    # the [-1,+1] window of words around the token
     for o in [-1,0,1]:
         if i+o >= 0 and i+o < len(sent):
-            word = sent[i+o][0]
-            featlist = getfeats(word, o)
-            features.extend(featlist)
-    
+            word_tuple = sent[i+o]
+            word_window = get_words_in_window(word_tuple, o)
+            features.extend(word_window)
+
+    # part of speech
+    pos = ('pos', sent[i][1])
+    features.append(pos)
+
+    # prop = ('prop', is_proper_case(sent[i][0]))
+    # features.append(prop)
+
     return dict(features)
+
+
+def generate_features_and_labels(sentences):
+    feats = []
+    labels = []
+
+    for sent in sentences:
+        for i in range(len(sent)):
+            sent_feats = word2features(sent,i)
+            feats.append(sent_feats)
+            labels.append(sent[i][-1])
+
+    return feats, labels
+
+
+def generate_tensor(vectorizer, sentences, fit=False):
+    feats, labels = generate_features_and_labels(sentences)
+
+    if fit:
+        tensor = vectorizer.fit_transform(feats)
+    else:
+        tensor = vectorizer.transform(feats)
+
+    return tensor, labels
+
 
 if __name__ == "__main__":
     # Load the training data
     train_sents = list(conll2002.iob_sents('esp.train'))
     dev_sents = list(conll2002.iob_sents('esp.testa'))
     test_sents = list(conll2002.iob_sents('esp.testb'))
-    
-    train_feats = []
-    train_labels = []
 
-    for sent in train_sents:
-        for i in range(len(sent)):
-            feats = word2features(sent,i)
-            train_feats.append(feats)
-            train_labels.append(sent[i][-1])
-
+    # Generate training tensor
     vectorizer = DictVectorizer()
-    X_train = vectorizer.fit_transform(train_feats)
+    X_train, train_labels = generate_tensor(vectorizer, train_sents, fit=True)
 
-    # TODO: play with other models
-    model = Perceptron(verbose=1)
+    # Train the model
+    # model = Perceptron(verbose=True, max_iter=20)
+    model = MLPClassifier(verbose=True, solver='lbfgs', max_iter=200)
     model.fit(X_train, train_labels)
 
-    test_feats = []
-    test_labels = []
+    # Generate test tensor (switch to test_sents for submission output)
+    X_test, test_labels = generate_tensor(vectorizer, dev_sents)
 
-    # switch to test_sents for your final results
-    for sent in dev_sents:
-        for i in range(len(sent)):
-            feats = word2features(sent,i)
-            test_feats.append(feats)
-            test_labels.append(sent[i][-1])
-
-    X_test = vectorizer.transform(test_feats)
+    # Generate labels using trained model
     y_pred = model.predict(X_test)
 
+
+    # wrongs = []
     j = 0
     print("Writing to results.txt")
     # format is: word gold pred
     with open("results.txt", "w") as out:
-        for sent in dev_sents: 
+        for sent in dev_sents:
             for i in range(len(sent)):
                 word = sent[i][0]
                 gold = sent[i][-1]
                 pred = y_pred[j]
-                j += 1
+                # if gold != pred:
+                #
+                # j += 1
                 out.write("{}\t{}\t{}\n".format(word,gold,pred))
         out.write("\n")
 
     print("Now run: python conlleval.py results.txt")
+
+
+
 
 
 
---
+++ wrongs.py
@@ -0,0 +1,19 @@
+from nltk.corpus import conll2002
+from ner import generate_features_and_labels
+dev_sents = list(conll2002.iob_sents('esp.testa'))
+
+feats, labels = generate_features_and_labels(dev_sents)
+
+with open("results-mlp-lbfgs-200-pos1.txt", "r") as f:
+    line_count = 0
+    wrong_count = 0
+    for line in f:
+        word, gold, pred = line.split()
+        if gold != pred:
+            print(line + str(feats[line_count]))
+            print()
+            wrong_count += 1
+        line_count += 1
+
+print("\nTotal number wrong: %d" % (wrong_count))
+
